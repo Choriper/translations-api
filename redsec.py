257078b12088553aed4f76ca9fa110b817a1a73a
jn_nicollas <117091369+Jnicollas@users.noreply.github.com>
2025-11-22 13:55:35 -0300

    Add REDSEC mode translations
    Added translations for different game modes including image URLs and labels in Portuguese and English.

---
+++ redsec.py
@@ -0,0 +1,27 @@
+# app/translations/redsec.py
+
+from typing import Dict, Any
+
+REDSEC_MODE_TRANSLATIONS: Dict[str, Dict[str, Any]] = {
+    "GraniteSquad": {
+        "image_url": "https://rip-bf.com/wp-content/uploads/2025/11/GraniteSquad0.png",
+        "labels": {
+            "pt-br": "BR QUARTETO - RESISTENCIA CA",
+            "en-us": "BATTLE ROYALE QUADS",
+        },
+    },
+    "GraniteGauntlet": {
+        "image_url": "https://rip-bf.com/wp-content/uploads/2025/11/REDSEC.png",
+        "labels": {
+            "pt-br": "ARENA",
+            "en-us": "Gauntlet",
+        },
+    },
+    "GraniteDuo": {
+        "image_url": "https://rip-bf.com/wp-content/uploads/2025/11/REDSEC.png",
+        "labels": {
+            "pt-br": "BR DUPLAS - RESISTENCIA DA CALIFÓRNIA",
+            "en-us": "BATTLE ROYALE DUOS",
+        },
+    },
+}
